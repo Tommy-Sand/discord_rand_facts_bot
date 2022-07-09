753fc97bce3caf60d8daf0d5ebc366ddc199ba2e
Tommmy Sandanasamy <rosansandanasamy@gmail.com>
2022-07-08 18:08:20 -0600

    Removed list var, fixed so api calls and bot works

--- src/main.py
+++ src/main.py
@@ -1,17 +1,14 @@
-import discord, requests
+import discord, requests, json
 from continuous_hosting import keep_alive
 
 def random_facts():
-    header = {"X-Api-Key": "Add-Your-API-key"}
+    header = {"X-Api-Key": "Enter your ninja-api key"}
     html_response = requests.get("https://api.api-ninjas.com/v1/facts?limit=1", headers=header)
     if html_response.status_code != 200:
         return "Something went wrong: We were unable to fetch a fact for you" 
-    else:
-        print(html_response.text)
-    return html_response.content
+    return json.loads(html_response.text)
 
-if(__name__ != '__main__'):
-    facts = []
+if(__name__ == '__main__'):
     client = discord.Client()
 
     @client.event
@@ -24,8 +21,7 @@ if(__name__ != '__main__'):
             return
         
         if message.content == 'gib fact':
-            if facts == []:
-                content = random_facts()
-            await message.channel.send(content[0]["fact"])
+            content = random_facts()
+            await message.channel.send(content[0]['fact'])
     keep_alive()
-    client.run('Add bot token')
+    client.run('Enter your token id')
